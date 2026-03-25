08c7c1e268d55d1b0997b6dff245e5ad76cd61bf
malthumkarramcharan12-web <malthumkarramcharan12@gmail.com>
2026-03-25 12:32:45 +0530

    Create players.py

---
+++ players.py
@@ -0,0 +1,7 @@
+players = [
+    {"name": "Virat Kohli", "base_price": 20},
+    {"name": "MS Dhoni", "base_price": 15},
+    {"name": "Rohit Sharma", "base_price": 18},
+    {"name": "Hardik Pandya", "base_price": 16},
+    {"name": "Jasprit Bumrah", "base_price": 17}
+]
